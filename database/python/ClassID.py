dd7c86a30efc718e0c700451ae20064d7d43a10c
Poyu <bruce920309@gmail.com>
2023-04-20 16:42:56 +0800

    新增各種資料集

---
+++ database/python/ClassID.py
@@ -0,0 +1,147 @@
+# fork from HeiTang/FCU-ClassID
+
+import requests
+import os
+import json
+import pandas as pd
+
+def GetDeptList(Dept_data,degree):
+    payload = {
+        "baseOptions":{
+            "lang":"cht",
+            "year":108,
+            "sms":2
+        },
+        "degree":degree
+    }
+    payload = json.dumps(payload)
+    url = "https://coursesearch04.fcu.edu.tw/Service/Search.asmx/GetDeptList"
+    r = Post(url,payload)
+
+    i = 0
+    while(True):   
+        try:
+            seq = r['d'][i]['name']
+            value = r['d'][i]['id']
+            Dept_data.setdefault(seq,value)
+            i=i+1
+        except:
+            return Dept_data
+    
+def GetUnitList(Unit_data,degree,deptId):
+    payload = {
+        "baseOptions":{
+            "lang":"cht",
+            "year":108,
+            "sms":2
+        },
+        "degree":degree,
+        "deptId":deptId
+    }
+
+    payload = json.dumps(payload)
+    url = "https://coursesearch04.fcu.edu.tw/Service/Search.asmx/GetUnitList"
+    r = Post(url,payload)
+
+    i = 0
+    while(True):   
+        try:
+            seq = r['d'][i]['name']
+            value = r['d'][i]['id']
+            Unit_data.setdefault(seq,value)
+            i=i+1
+        except:
+            return Unit_data
+
+def GetClassList(Class_data,degree,unitId):
+    payload = {
+        "baseOptions":{
+            "lang":"cht",
+            "year":108,
+            "sms":2
+        },
+        "degree":degree,
+        "unitId":unitId
+    }
+
+    payload = json.dumps(payload)
+    url = "https://coursesearch04.fcu.edu.tw/Service/Search.asmx/GetClassList"
+    r = Post(url,payload)
+
+    i = 0
+    while(True):   
+        try:
+            seq = r['d'][i]['name']
+            value = r['d'][i]['id']
+            Class_data.setdefault(seq,value)
+            i=i+1
+        except:
+            return Class_data
+
+def Post(url,payload):
+
+    headers = {
+        "Accept": "*/*",
+        "Accept-Language": 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6',
+        "DNT": "1",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Content-Type":"application/json; charset=UTF-8",
+        "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3573.0 Safari/537.36",
+    }
+
+    r = requests.post(url,headers=headers,data = payload)  
+    r = r.text                                             
+    r = r.replace('\\"','"' )            # 將 \" 過濾成 "
+    r = r.replace('"d":"[{','"d": [{' )   
+    r = r.replace('}]"}','}]}')
+    r = json.loads(r)    
+    return r                                                         
+
+def Show(dict):
+    list = dict.keys()
+    for i in list:
+        print(dict[i] + ":" + i)
+    
+
+if __name__ == '__main__': 
+
+    dir = os.path.dirname(__file__) + "\\data\\id\\"
+    
+    # 所有學院
+    Dept_data = {}
+    for degree in 1,3,4,5:
+        GetDeptList(Dept_data,degree)
+    print("\n## 學院列表：（" + str(len(Dept_data)) + " 筆資料）")
+    # Show(Dept_data)
+    dataframe = pd.Series(Dept_data, index = Dept_data.keys()) 
+    dataframe.to_csv(dir + "csv\\Dept_data.csv")
+    with open(dir + "json\\Dept_data.json", 'w', encoding='utf-8') as f:
+        d = json.dumps(Dept_data, ensure_ascii=False)
+        f.write(d)
+
+    # 所有系所
+    Unit_data = {}
+    for degree in 1,3,4,5:
+        for deptId in Dept_data.values():
+            GetUnitList(Unit_data,degree,deptId)
+    print("\n## 系所列表：（" + str(len(Unit_data)) + " 筆資料）")
+    # Show(Unit_data)
+    dataframe = pd.Series(Unit_data, index = Unit_data.keys()) 
+    dataframe.to_csv(dir + "csv\\Unit_data.csv")
+    with open(dir + "json\\Unit_data.json", 'w', encoding='utf-8') as f:
+        d = json.dumps(Unit_data, ensure_ascii=False)
+        f.write(d)
+
+    # 所有班級
+    Class_data = {}
+    for degree in 1,3,4,5:
+        for unitId in Unit_data.values():
+            GetClassList(Class_data,degree,unitId)
+    print("\n## 班級列表：（" + str(len(Class_data)) + " 筆資料）")
+    # Show(Class_data)
+    dataframe = pd.Series(Class_data, index = Class_data.keys()) 
+    dataframe.to_csv(dir + "csv\\Class_data.csv")
+    with open(dir + "json\\Class_data.json", 'w', encoding='utf-8') as f:
+        d = json.dumps(Class_data, ensure_ascii=False)
+        f.write(d)
---
+++ database/python/CourseDump.py
@@ -0,0 +1,92 @@
+# fork from HeiTang/FCU-CourseData
+import requests
+import json, os, sys
+
+SMS = [1, 2, 3, 4]    # 1:上學期 2:下學期 3:暑修上 4:暑修下
+DEGREE = [1, 3, 4, 5] # 1:大學部 3:碩士班 4:博士班 5:進修班
+
+class CourseDump:
+    def __init__(self, year, sms):
+        self.headers = {
+            "Content-Type":"application/json; charset=UTF-8",
+            "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3573.0 Safari/537.36",
+        }
+
+        self.payload = {
+            "baseOptions": {
+                "lang": "cht",
+                "year": year,
+                "sms": sms
+            }
+        }
+    def getUnitList(self, degree, dept_id):
+        self.payload["unit"] = ""
+        url = 'https://coursesearch03.fcu.edu.tw/Service/Search.asmx/GetUnitList'
+        r = requests.post(url, headers = self.headers, data = json.dumps(self.payload))
+        r = self.stringProcess(r.text)
+        unit = json.loads(r)['d']
+        return unit
+
+    def getDeptList(self):
+        self.payload["degree"] = "1"
+        url = 'https://coursesearch03.fcu.edu.tw/Service/Search.asmx/GetDeptList'
+        r = requests.post(url, headers = self.headers, data = json.dumps(self.payload))
+        r = self.stringProcess(r.text)
+        dept = json.loads(r)['d']
+        return dept
+
+    def getCourseList(self, degree, dept_id):
+        self.payload["typeOptions"] = {
+            "degree": degree,
+            "deptId": dept_id,
+            "unitId": "*",
+            "classId": "*"
+        }
+
+        url = "https://coursesearch04.fcu.edu.tw/Service/Search.asmx/GetType1Result"
+        r = requests.post(url, headers = self.headers, data = json.dumps(self.payload))
+        r = self.stringProcess(r.text)
+        r = json.loads(r)                                                            # JSON -> dict
+        return r
+
+    def saveFile(self, data, path, file_name):
+        with open(os.path.join(path, file_name), 'w', encoding='utf-8') as f:
+            f.write(data)
+            f.flush()
+            f.close() 
+
+    def stringProcess(self, string):
+        string = string.replace('\\"', '"' )            # 將 \" 過濾成 "
+        string = string.replace('\\\"', '"')            # 將 \\" 過濾成 "
+        string = string.replace('"d":"{', '"d": {' )   
+        string = string.replace('"d":"[{', '"d": [{' )
+        string = string.replace(']}"}', ']}}')
+        string = string.replace('}]"}', '}]}')
+        return string
+
+if __name__ == '__main__': 
+    # years = sys.argv[1].split(',')
+    years = [110, 109, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100]
+    for year in years:
+        # 確認資料夾是否存在，否則建立學年度資料夾
+        dir = os.path.dirname(__file__)
+        json_path = dir + "\\data\\class" + "\\{}\\".format(str(year))
+        if not os.path.exists(json_path):
+            os.mkdir(json_path)
+    
+        for sms in SMS:
+            course = CourseDump(year, sms)
+            dept = course.getDeptList()
+            
+            if dept != '[]':                     # 確認是否有開放查詢
+                for degree in DEGREE:
+                    for i in range(len(dept)):
+                        dept_id = dept[i]['id']
+                        raw_data = course.getCourseList(degree, dept_id)
+                        # print(raw_data['d']['items'][0]['scr_selcode'])
+                        # print(json.dumps(raw_data, ensure_ascii = False, indent = 4, separators = (',', ': ')))
+                        file_name = '{}{}-{}-{}.json'.format(str(year), str(sms), str(degree), dept_id)
+                        course.saveFile(json.dumps(raw_data, ensure_ascii = False, indent = 4, separators = (',', ': ')), json_path, file_name)
+                        print(f"+ {file_name}")
+            else:
+                print("- {}{}: No data".format(str(year), str(sms)))
