e4640b5a745efbad7956b7b5becfee58d612c836
Poyu <bruce920309@gmail.com>
2023-04-27 00:33:14 +0800

    新增 111~100 前置課程資料

--- database/CourseInfoDump.py
+++ database/CourseInfoDump.py
@@ -1,4 +1,5 @@
-import json
+import json, re
+from threading import Thread
 import concurrent.futures
 import os
 import requests
@@ -6,59 +7,39 @@ from bs4 import BeautifulSoup
 import time
 
 class PreCourse:
-    def __init__(self, json_file, max_t):
+    def __init__(self, json_file):
         self.json_file = json_file
         self.year = self.json_file[0:3]
         self.sms = self.json_file[3]
         self.dept = self.json_file[7:9]
         self.dir = os.path.dirname(__file__)
-        self.max_t = max_t
+        
+        self.loadJson()
+        self.sortData()
 
     def loadJson(self):
         with open(self.dir + "\\data\\url\\" + self.year + "\\" + self.json_file, 'r', encoding='utf-8') as f:
             data = json.loads(f.read())
-        return data
+        self.json_data = data
     
-    def splitData(self):
-        data = self.loadJson()
-        if data == []:
-            return
-        dataLists = []
-        each = int(len(data) / self.max_t)
-        last = int(len(data) % self.max_t)
-        index = 0
-        for i in range(0, self.max_t):
-            temp = []
-            extra = 1 if last > 0 else 0
-            for j in range(0, each + extra):
-                temp.append(data[index + j])
-            if last > 0:
-                last -= 1
-            index += each + extra
-            dataLists.append(temp)
-        return dataLists
-    
-    def multitip(self, dataLists):
-        # 建立多執行緒任務
-        with concurrent.futures.ThreadPoolExecutor(max_workers = self.max_t) as executor:
-            executor.map(self.sortData, dataLists)
-    
-    def sortData(self, jsonDataList):
+    def sortData(self):
         output = []
-        for i in range(0, len(jsonDataList)):
-            JsonData = jsonDataList[i]
-            if JsonData == []:
+        for i in range(0, len(self.json_data)):
+            data = self.json_data[i]
+            if data == []:
                 continue
-            self.scr_selcode = JsonData['scr_selcode']
-            self.cls_id = JsonData['cls_id']
-            dataList = self.requestData(JsonData['url'])
-            # for i in range(0, len(dataList)):
-            #     for data in dataList[i]:
-            #         for key in data:
-            #             output[key] = data[key]
-            # print(output)
-            # self.saveJson(output, self.scr_selcode + '-' + self.cls_id + '.json')
-            output.append(dataList)
+            self.scr_selcode = data['scr_selcode']
+            self.cls_id = data['cls_id']
+            request_data_list = self.requestData(data['url'])
+            if request_data_list == []:
+                continue
+            temp_dict = {}
+            temp_dict['scr_selcode'] = self.scr_selcode
+            temp_dict['cls_id'] = self.cls_id
+            temp_dict['PreCourse'] = request_data_list
+            for PreCourse in temp_dict['PreCourse']:
+                PreCourse['pre_subid3'] = PreCourse['pre_subid3'].replace(' ', '')
+            output.append(temp_dict)
         self.saveJson(output, str(self.json_file))
     
     def requestData(self, url):
@@ -84,18 +65,18 @@ class PreCourse:
             'course_id': course_id
         }
         urlList = [
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getCourseInfo',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getCourseDescr',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getCourseInfo',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getCourseDescr',
             'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getPreCourse',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getCorrelation',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getLearnOutcome',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getEvoluationInfo',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getTextbook',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getSoftWare',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getReference',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getTeachSchedule',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getEvoluationMethod',
-            'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getTeachMethod'
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getCorrelation',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getLearnOutcome',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getEvoluationInfo',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getTextbook',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getSoftWare',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getReference',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getTeachSchedule',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getEvoluationMethod',
+            # 'https://service120-sds.fcu.edu.tw/W320104/action/getdata.aspx/getTeachMethod'
         ]
         raw_dataList = []
         for url in urlList:
@@ -105,43 +86,33 @@ class PreCourse:
                 headers=headers,
                 json=json_data,
             )
-            string = response.text
-
+            string = json.loads(response.text)['d']
+            if (string != ''):
+                raw_dataList.append(json.loads(string)[0])
+        
         print(self.scr_selcode + '-' + self.cls_id + ' ' + 'requests success')
-        self.saveJson(raw_dataList, self.scr_selcode + '-' + self.cls_id + '.json')
-        os._exit(0)
         return raw_dataList
     
-    def stringProcess(self, string):
-        string = string.replace('"[', '[')
-        string = string.replace(']"', ']')
-        string = string.replace('\\r\\n', '')
-        # string = string.replace('"d":"{', '"d": {' )   
-        # string = string.replace('"d": "[{', '"d": [{' )
-        # string = string.replace('}]"}', '}]}')
-        # string = string.replace(']}"}', ']}}')
-        return string
-    
     def saveJson(self, data, file_name):
-        dir = os.path.dirname(__file__) + "\\data\\courseInfo\\111\\"
+        dir = os.path.dirname(__file__) + "\\data\\courseInfo\\" + self.year + "\\"
         if not os.path.exists(dir):
             os.mkdir(dir)
-        # dir = dir + self.json_file.split('.')[0] + "\\"
-        # if not os.path.exists(dir):
-            # os.mkdir(dir)
-        with open(dir + file_name, 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii = False, indent = 4, separators = (',', ': '))
-        print(file_name + ' save success')
+        if data != []:
+            with open(dir + file_name, 'w', encoding='utf-8') as f:
+                json.dump(data, f, ensure_ascii = False, indent = 4, separators = (',', ': '))
+                f.flush()
+                f.close()
+            print(file_name + ' save success')
         
 if __name__ == '__main__':
-    dir = os.path.dirname(__file__) + "\\data\\url\\111\\"
-    file_list = ["1111-1-CI.json"]
-    # file_list = os.listdir(dir)
-    start_time = time.time()
-    for file in file_list:
-        pre = PreCourse(file, 10)
-        dataLists = pre.splitData()
-        # pre.multitip(dataLists)
-        pre.sortData(dataLists[0])
-    end_time = time.time()
-    print('執行時間：' + str(end_time - start_time) + '秒')
+    # 資料範圍
+    years = [110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100]
+    for year in years:
+        dir = os.path.dirname(__file__) + "\\data\\url\\" + str(year) + "\\"
+        file_list = os.listdir(dir)
+        start_time = time.time()
+        # multi-threading
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            executor.map(PreCourse, file_list)
+        end_time = time.time()
+        print('執行時間：' + str(end_time - start_time) + '秒')
