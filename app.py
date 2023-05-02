f46be9ce10fe8c29417ed5faf233433c5531b220
kokorosawa <90631564+kokorosawa@users.noreply.github.com>
2023-05-02 16:30:43 +0800

    front on flask

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask,render_template
-app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello, World!"
