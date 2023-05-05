6da4e2b0cb67a6b2f63180427cae3fafc0526735
kokorosawa <matt920404@gmail.com>
2023-05-06 04:38:26 +0800

    backend_init

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from app.route import hello_world, index
+from app.route import hello_world, index,login,register
 
 def create_app():
     app = Flask(__name__)
-    app.add_url_rule('/', '/', hello_world)
-    app.add_url_rule('/index', 'index', index)
+    app.add_url_rule('/', '/', index)
+    app.add_url_rule('/index.html', 'index', index)
+    app.add_url_rule('/login.html', 'login', login)
+    app.add_url_rule('/register.html', 'register', register)
     return app
--- flask/app/route.py
+++ flask/app/route.py
@@ -5,3 +5,9 @@ def hello_world():
 
 def index():
     return render_template('index.html') 
+
+def login():
+    return render_template('login.html')
+
+def register():
+    return render_template('register.html')
