aed2dba778d9f930445cdc3590340770656f29d2
kokorosawa <matt920404@gmail.com>
2023-05-07 05:15:26 +0800

    result.html

--- flask/app/index.py
+++ flask/app/index.py
@@ -1,8 +1,9 @@
 from flask import  request,render_template,flash,abort,url_for,redirect,Flask
 import MySQLdb
-import sys,io
+import sys,io,json
 from flask_cors import CORS,cross_origin
 sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -14,13 +15,18 @@ def index():
 
 @app.route('/submit',methods=['POST', 'GET'])
 def submit():
-     dic = {}
-     if request.method == 'POST':
-        dic['Degree'] = request.form['Degree']
-        dic['College'] = request.form['College']
-        return str(dic)
-     else:
+    if request.method == 'POST':
+        Class_id = request.form['Classid']
+        return redirect(url_for('result',Classid = Class_id))
+    else:
         return render_template('index.html')
+    
+@app.route('/result/<Classid>')
+def result(Classid):
+    r = requests.get(f"http://127.0.0.1:5000/API/?year=111&sms=1&cls_id={Classid}")
+    r = json.loads(r.text)
+    print(r)
+    return render_template('result.html',data = r)
      
 # @app.route('/ajax/degree')
 # def degree():
