4a1331bb4217b8c86889814de3202049a312a697
Poyu <bruce920309@gmail.com>
2023-05-07 16:40:32 +0800

    新增 getIdList

--- API/main.py
+++ API/main.py
@@ -1,25 +1,11 @@
-from flask import Flask, request, jsonify
-from APIDataBase import APIDataBase
+from flask import Flask
+from getCourse import getCourse_blp
+from getIdList import getIdList_blp
 
 app = Flask(__name__)
-db = APIDataBase('localhost', 3306, 'root', 'fcu')
 
-# getCourse
-@app.route('/API/', methods=['GET'])
-def getCourse():
-    year = request.args.get('year')
-    sms = request.args.get('sms')
-    # degree = request.args.get('degree')
-    # dept = request.args.get('dept')
-    # unit = request.args.get('unit')
-    cls_id = request.args.get('cls_id')
-    print(year, sms, cls_id)
-    if year == None or sms == None or cls_id == None:
-        return jsonify({'error': '參數錯誤'})
-    data = db.execSelect('SELECT * FROM `' + year + sms + '_course`' + ' WHERE `cls_id` = ' + '\'' + cls_id + '\'')
-    if data == []:
-        return jsonify({'error': '查無資料'})
-    return jsonify(data)
+app.register_blueprint(getCourse_blp, url_prefix = '/API')
+app.register_blueprint(getIdList_blp, url_prefix = '/API')
 
 if __name__ == '__main__':
     app.run(debug=True)
