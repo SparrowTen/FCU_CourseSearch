19fa9199d246c372c14f3df58a9a5d035b86b42d
Poyu <42506064+poyu39@users.noreply.github.com>
2023-05-08 17:44:51 +0800

    新增加選限制 (衝堂) 和 關注

--- API/Course.py
+++ API/Course.py
@@ -4,6 +4,74 @@ from APIDataBase import APIDataBase
 Course_blp = Blueprint('Course', __name__)
 db = APIDataBase('localhost', 3306, 'root', 'fcu')
 
+# 新增課程
+def addCourse(year, sms, std_id, scr_selcode, cls_id, focus = False):
+    # 取得原始時間資料和課程名稱
+    r = db.execSelect(f"SELECT `sub_name`, `scr_period` FROM {year}{sms}_course WHERE `scr_selcode` = \'{scr_selcode}\' AND `cls_id` = \'{cls_id}\'")
+    rawStr = str(r[0]['scr_period'])
+    sub_name = r[0]['sub_name']
+    
+    # 取得星期幾
+    dayKeys = ['(一)', '(二)', '(三)', '(四)', '(五)', '(六)', '(日)']
+    rawtimeList = []
+    for dayKey in dayKeys:
+        this_day_count = rawStr.count(dayKey)
+        for i in range(0, this_day_count):
+            rawtimeList.append(rawStr[rawStr.find(dayKey, i):rawStr.find(dayKey, i) + 8])
+    
+    # 處理時間資料
+    for timeStr in rawtimeList:
+        # 取得學生的 curr_id
+        r = db.execSelect(f"SELECT `curr_id` FROM {year}{sms}_student WHERE `std_id` = \'{std_id}\'")
+        curr_id = r[0]['curr_id']
+        
+        # 取得星期幾
+        day = timeStr[1]
+        
+        # 取得該星期幾的課表
+        r = db.execSelect(f"SELECT * FROM {year}{sms}_curriculum WHERE `curr_id` = \'{curr_id}\' AND `day` = \'{day}\'")
+        if r == []:
+            db.exec(f"REPLACE INTO {year}{sms}_curriculum (`curr_id`, `day`) VALUES ('{curr_id}', '{day}')")
+        timeList = {}
+        for i in range(1, 15):
+            timeList[i] = r[0][str(i)]
+            # timeList.append(r[0][str(i)])
+        
+        # 取得第幾節
+        if '-' in timeStr:
+            start_time = timeStr.split(')')[1][0:2]
+            end_time = timeStr.split(')')[1][3:5]
+            for i in range(int(start_time), int(end_time) + 1):
+                if timeList[i] != '' and focus == False:
+                    return False
+                if focus:
+                    timeList[i] = timeList[i] + f" f{sub_name}{scr_selcode}{cls_id}"
+                else:
+                    timeList[i] = f"{sub_name}{scr_selcode}{cls_id}"
+        else:
+            time = int(timeStr.split(')')[1][0:2])
+            # print(time)
+            if timeList[time] != '' and focus == False:
+                return False
+            if focus:
+                timeList[time] = timeList[time] + f" f{sub_name}{scr_selcode}{cls_id}"
+            else:
+                timeList[time] = f"{sub_name}{scr_selcode}{cls_id}"
+    
+        dataDict = {}
+        # print(timeList)
+        for i in range(1, 15):
+            if timeList[i] == '':
+                dataDict[i] = ''
+            else:
+                dataDict[i] = timeList[i][0:-1]
+        
+        # 更新課表
+        for i in range(1, 15):
+            db.exec(f"UPDATE {year}{sms}_curriculum SET `{i}` = \'{dataDict[i]}\' WHERE `curr_id` = \'{curr_id}\' AND `day` = \'{day}\'")
+    
+    return True
+
 @Course_blp.route('/add', methods=['GET', 'POST'])
 def add():
     year = "111"
@@ -12,37 +80,10 @@ def add():
     scr_selcode = request.values['scr_selcode']
     cls_id = request.values['cls_id']
     
-    r = db.execSelect(f"SELECT `sub_name`, `scr_period` FROM {year}{sms}_course WHERE `scr_selcode` = \'{scr_selcode}\' AND `cls_id` = \'{cls_id}\'")
-    timeStr = r[0]['scr_period'].split(' ')[0]
-    sub_name = r[0]['sub_name']
-    # print(sub_name)
-    
-    day = timeStr[1]
-    timeList = [None, None, None, None, None, None, None, None, None, None, None, None, None, None]
-    
-    if '-' in timeStr:
-        start_time = timeStr.split(')')[1][0:2]
-        end_time = timeStr.split(')')[1][3:5]
-        for i in range(int(start_time), int(end_time) + 1):
-            timeList[i - 1] = f"{sub_name} {scr_selcode} {cls_id}"
+    if addCourse(year, sms, std_id, scr_selcode, cls_id):
+        return jsonify({'success': '加選成功'})
     else:
-        time = timeStr.split(')')[0:2]
-        timeList[time - 1] = f"{sub_name} {scr_selcode} {cls_id}"
-    
-    dataDict = {}
-    for i in range(0, 14):
-        if timeList[i] == None:
-            dataDict[i + 1] = ""
-        else:
-            dataDict[i + 1] = timeList[i][0:-1]
-    
-    r = db.execSelect(f"SELECT `curr_id` FROM {year}{sms}_student WHERE `std_id` = \'{std_id}\'")
-    curr_id = r[0]['curr_id']
-    
-    for i in range(1, 15):
-        db.exec(f"UPDATE {year}{sms}_curriculum SET `{i}` = \'{dataDict[i]}\' WHERE `curr_id` = \'{curr_id}\' AND `day` = \'{day}\'")
-    
-    return ('', 200)
+        return jsonify({'error': '加選失敗，衝堂'})
 
 @Course_blp.route('/focus', methods=['POST'])
 def focus():
@@ -51,39 +92,11 @@ def focus():
     std_id = request.values['std_id']
     scr_selcode = request.values['scr_selcode']
     cls_id = request.values['cls_id']
-    
-    r = db.execSelect(f"SELECT `sub_name`, `scr_period` FROM {year}{sms}_course WHERE `scr_selcode` = \'{scr_selcode}\' AND `cls_id` = \'{cls_id}\'")
-    timeStr = r[0]['scr_period'].split(' ')[0]
-    sub_name = r[0]['sub_name']
-    
-    day = timeStr[1]
-    timeList = [None, None, None, None, None, None, None, None, None, None, None, None, None, None]
-    
-    if '-' in timeStr:
-        start_time = timeStr.split(')')[1][0:2]
-        end_time = timeStr.split(')')[1][3:5]
-        for i in range(int(start_time), int(end_time) + 1):
-            timeList[i - 1] = f"{sub_name} {scr_selcode} {cls_id}"
+    if addCourse(year, sms, std_id, scr_selcode, cls_id, focus=True):
+        return jsonify({'success': '關注成功'})
     else:
-        time = timeStr.split(')')[0:2]
-        timeList[time - 1] = f"{sub_name} {scr_selcode} {cls_id}"
+        return jsonify({'error': '關注失敗，衝堂'})
     
-    dataDict = {}
-    focus = "f "
-    for i in range(0, 14):
-        if timeList[i] == None:
-            dataDict[i + 1] = ""
-        else:
-            dataDict[i + 1] = focus + timeList[i][0:-1]
-    
-    r = db.execSelect(f"SELECT `curr_id` FROM {year}{sms}_student WHERE `std_id` = \'{std_id}\'")
-    curr_id = r[0]['curr_id']
-    
-    for i in range(1, 15):
-        db.exec(f"UPDATE {year}{sms}_curriculum SET `{i}` = \'{dataDict[i]}\' WHERE `curr_id` = \'{curr_id}\' AND `day` = \'{day}\'")
-    
-    return ('', 200)
-
 @Course_blp.route('/getCurriculum', methods=['POST'])
 def getCurriculum():
     year = "111"
@@ -124,5 +137,5 @@ def getCurriculum():
                 dayDict[i] = r[0][f"{i}"]
                 
             currDict[day] = dayDict
-        print(currDict)
+        # print(currDict)
     return currDict
